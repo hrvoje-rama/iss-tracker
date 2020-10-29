10561590e35301130bf077ec0d6758a104d877dd
unknown <hrvojeramljak@gmail.com>
2020-10-29 11:18:15 +0100

    Updates

---
+++ precache-manifest.bde86106596ecd327b4598538bb90e59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df74719e82dd3af2564b4cf0989e2f6f",
+    "url": "/iss-tracker/index.html"
+  },
+  {
+    "revision": "c87db675ecb969d91826",
+    "url": "/iss-tracker/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6d48a9cbda3fcc3ed16b",
+    "url": "/iss-tracker/static/css/main.602bba85.chunk.css"
+  },
+  {
+    "revision": "c87db675ecb969d91826",
+    "url": "/iss-tracker/static/js/2.6f5be5cf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/iss-tracker/static/js/2.6f5be5cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d48a9cbda3fcc3ed16b",
+    "url": "/iss-tracker/static/js/main.bcd5290c.chunk.js"
+  },
+  {
+    "revision": "6bacf1c62c819b9359dc",
+    "url": "/iss-tracker/static/js/runtime-main.09c46729.js"
+  },
+  {
+    "revision": "f066e4ab5adcca350ab88f48775376d3",
+    "url": "/iss-tracker/static/media/SF Distant Galaxy AltOutline.f066e4ab.woff"
+  }
+]);
